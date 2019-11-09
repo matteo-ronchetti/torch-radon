8d37b1f9188c47fae0bce72b5e6052c39c1d4cd9
Matteo on g4 AWS instance <mttronchetti@gmail.com>
2019-11-09 20:50:17 +0000

    fixed setup

--- torch_radon/setup.py
+++ torch_radon/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, Extension
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(name='torch_radon',
+      py_modules=['torch_radon'],
       ext_modules=[
           CUDAExtension('torch_radon_cuda', [
               'radon.cpp',
