a4c928849b121362adf7a323c43b3ad28e6eeb42
Matteo on g4 AWS instance <mttronchetti@gmail.com>
2019-11-10 14:05:02 +0000

    fixed setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 setup(name='torch_radon',
       py_modules=['torch_radon'],
@@ -7,6 +11,8 @@ setup(name='torch_radon',
           CUDAExtension('torch_radon_cuda', [
               'src/radon.cpp',
               'src/radon_cuda.cu',
-          ])
+          ],
+          include_dirs=['include'])
+          #extra_compile_args={"cxx": [], "nvcc": ["-I" + os.path.join(dir_path, "include")]})
       ],
       cmdclass={'build_ext': BuildExtension})
