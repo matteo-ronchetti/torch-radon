cfc805839064389924d61306dcf6b87534a381ad
Matteo on g4 AWS instance <mttronchetti@gmail.com>
2019-11-10 15:24:56 +0000

    .

--- benchmark.py
+++ benchmark.py
@@ -7,22 +7,34 @@ from torch_radon import Radon
 
 device = torch.device('cuda')
 
-def bench(batch_size, image_size, n_angles=180):
+def bench(batch_size, image_size, n_angles=180, sample_size=50):
     x = generate_random_images(batch_size, image_size)
     angles = np.linspace(0, 2 * np.pi, n_angles).astype(np.float32)
+    
+    astra = AstraWrapper(angles)
+    s = time.time()
+    for i in range(sample_size):
+        _ = astra.forward(x)
+    e = time.time()
+    astra_time = (e-s)/sample_size  
+    #TODO clear astra
+    
+    
     radon = Radon(image_size).to(device)
     x = torch.FloatTensor(x).to(device)
     angles = torch.FloatTensor(angles).to(device)
 
     s = time.time()
-    for i in range(50):
-        our_fp = radon.forward(x, angles)
+    for i in range(sample_size):
+        _ = radon.forward(x, angles)
     e = time.time()
+    our_time = (e-s)/sample_size  
     
-    return (e-s)/50
+    return our_time, astra_time
 
-for image_size in [64, 128, 256, 512]:
+for image_size in [64, 128]: #, 256, 512]:
     for n_angles in [180]:
         for batch_size in [1, 8, 16, 32, 64, 128, 256, 512]:
-            print(image_size, n_angles, batch_size, batch_size/bench(batch_size, image_size, n_angles))
+            our_time, astra_time = bench(batch_size, image_size, n_angles)
+            print(image_size, n_angles, batch_size, batch_size/our_time, batch_size/astra_time, astra_time/our_time)
         print("===========")
---
+++ tests/test_gradient.py
@@ -0,0 +1,54 @@
+from utils import generate_random_images, relative_error
+from nose.tools import assert_less
+import torch
+import numpy as np
+from torch_radon import Radon
+
+
+device = torch.device('cuda')
+
+params = []
+for batch_size in [1, 8, 16, 32, 64, 128, 256, 512]:
+    for image_size in [64, 128, 256, 512]:
+        params.append((device, batch_size, image_size))
+        
+@parameterized(params)
+def test_error(device, batch_size, image_size, n_angles=180):
+    # generate random images
+    x = generate_random_images(batch_size, image_size)
+
+    angles = np.linspace(0, 2 * np.pi, n_angles).astype(np.float32)
+
+    # astra
+    astra = AstraWrapper(angles)
+
+    astra_fp_id, astra_fp = astra.forward(x)
+    astra_bp = astra.backproject(astra_fp_id, image_size, batch_size)
+
+    # our implementation
+    radon = Radon(image_size).to(device)
+    x = torch.FloatTensor(x).to(device)
+    angles = torch.FloatTensor(angles).to(device)
+
+    our_fp = radon.forward(x, angles)
+    our_bp = radon.backprojection(our_fp, angles)
+
+    forward_error = relative_error(astra_fp, our_fp.cpu().numpy())
+    back_error = relative_error(astra_bp, our_bp.cpu().numpy())
+
+    # TODO clear astra
+    
+    print(batch_size, image_size, forward_error, back_error)
+    assert_less(forward_error, 1e-2)
+    assert_less(back_error, 5e-3)
+    
+# class TestBackend(TestCase):
+#     def test_backend(self):
+#         device = torch.device('cuda')
+
+#         for batch_size in [1, 8, 16, 32, 64, 128, 256, 512]:
+#             for image_size in [64, 128, 256, 512]:
+#                 forward_error, back_error = compute_error(device, batch_size, image_size)
+#                 print(batch_size, image_size, forward_error, back_error)
+#                 self.assertLess(forward_error, 1)
+#                 self.assertLess(back_error, 1)
