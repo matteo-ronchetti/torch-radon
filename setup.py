434ffc48516ec092cb7d505b57542d96e3ef5232
Matteo Ronchetti <mttronchetti@gmail.com>
2019-11-14 12:07:24 +0100

    finished packaginf

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='torch_radon',
+      version="0.0.1",
+      author="Matteo Ronchetti",
+      author_email="mttronchetti@gmail.com",
+      description="Radon transform in Pytorch",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/matteo-ronchetti/torch-radon",
+
       packages=['torch_radon'],
       package_dir={'torch_radon': './torch_radon'},
       ext_modules=[
@@ -16,4 +25,11 @@ setup(name='torch_radon',
                         extra_link_args=['-Wl,--strip-all']
                         )
       ],
-      cmdclass={'build_ext': BuildExtension})
+      cmdclass={'build_ext': BuildExtension},
+      zip_safe=False,
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Operating System :: POSIX :: Linux",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+      ]
+      )
