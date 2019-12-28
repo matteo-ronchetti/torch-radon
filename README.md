c2e2844e6226af16110bfba77c227197dc8a386f
matteo <mttronchetti@gmail.com>
2019-12-28 15:46:03 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# Radon transform in Pytorch
+# Computational Tomography in Pytorch
+This package is a CUDA implementation of transforms needed for
+working with computed tomography in Pytorch. Main features:
+ - All operations work directly on Pytorch GPU tensors.
+ - Forward and back projections are differentiable and integrated with Pytorch `.backward()`.
+ - Faster than Astra Toolbox, way faster if you consider that this implementation doesn't require to copy intermediate results to CPU
+
+## Example
+Simple example that uses Pytorch models to filter both the sinogram and the image
+```python
+import torch
+import torch.nn as nn
+import numpy as np
+from torch_radon import Radon
+
+batch_size = 8
+n_angles = 64
+image_size = 128
+
+device = torch.device('cuda')
+
+# instantiate Radon transform and loss
+radon = Radon(image_size).to(device)
+criterion = nn.L1Loss()
+
+# Instantiate a model for the sinogram and one for the image
+channels = 4
+sino_model = nn.Conv2d(1, channels, 5, padding=2).to(device)
+image_model = nn.Conv2d(channels, 1, 3, padding=1).to(device)
+
+# instantiate measuring angles
+angles = torch.FloatTensor(np.linspace(0, np.pi, n_angles)).to(device)
+
+# create empty images
+x = torch.FloatTensor(batch_size, 1, image_size, image_size).to(device)
+
+# forward projection
+sinogram = radon.forward(x, angles)
+
+# apply sino_model to sinograms
+sinogram = sino_model(sinogram)
+
+# back projection
+backprojected = radon.backprojection(sinogram, angles)
+
+# apply image_model to backprojected images
+y = image_model(backprojected)
+
+# compute image reconstruction error
+loss = criterion(y, x)
+
+# backward works as usual
+loss.backward()
+```
+
+## Installation
+### Precompiled packages
+TODO
+### Build from source
+You need to have installed CUDA and Pytorch, then run:
+```shell script
+git clone https://github.com/matteo-ronchetti/torch-radon.git
+cd torch-radon
+make install
+```
+
+## Testing
+Install testing dependencies with `pip install -r test_requirements.txt`
+then test with:
+```shell script
+nosetests tests/
+```
--- sample.py
+++ sample.py
@@ -1,76 +1,76 @@
-from tests.astra_wrapper import AstraWrapper
-from tests.utils import generate_random_images, relative_error, circle_mask
-import cv2
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
-from torch_radon import Radon, RadonNoiseGenerator
-
-
-def normalize(x):
-    x -= np.min(x)
-    x /= np.max(x)
-    x *= 255
-    return x.astype(np.uint8)
-
-
-device = torch.device('cuda')
-
-batch_size = 16
-image_size = 128
-n_angles = 128
-
-x = cv2.resize(cv2.imread("phantom.png", 0), (image_size, image_size), interpolation=cv2.INTER_CUBIC)
-x = x.astype(np.float32) / 255
-# x = generate_random_images(batch_size, image_size)
-angles = np.linspace(0, np.pi, n_angles).astype(np.float32)
-
-astra = AstraWrapper(angles)
-a_rec = astra.fbp(x)
-a_rec *= circle_mask(image_size)
-# a_id, a_y = astra.forward(x)
-# a_bp = astra.backproject(a_id, image_size, batch_size)
-
-with torch.no_grad():
-    radon = Radon(image_size).to(device)
-    x = torch.FloatTensor(x).view(1, image_size, image_size)
-    angles = torch.FloatTensor(angles).to(device)
-    x_ = x.to(device)
-    y = radon.forward(x_, angles)
-    y_ = radon.filter_sinogram(y)
-    z = radon.backprojection(y_, angles)
-
-print(z.size())
-z *= np.pi / n_angles
-# a_rec /= np.max(a_rec)
-print(torch.max(z[0]).item(), np.max(a_rec))
-print(np.linalg.norm(z[0].cpu().numpy() - a_rec) / np.linalg.norm(a_rec))
-plt.imshow(z[0].cpu().numpy() - a_rec)
-plt.figure()
-plt.imshow(a_rec)
-plt.show()
-# print(torch.mean((y - y_) ** 2))
-# print(torch.max(y), torch.max(y_))
-# plt.imshow(y[0].cpu().numpy())
+# from tests.astra_wrapper import AstraWrapper
+# from tests.utils import generate_random_images, relative_error, circle_mask
+# import cv2
+# import numpy as np
+# import torch
+# import matplotlib.pyplot as plt
+# from torch_radon import Radon, RadonNoiseGenerator
+#
+#
+# def normalize(x):
+#     x -= np.min(x)
+#     x /= np.max(x)
+#     x *= 255
+#     return x.astype(np.uint8)
+#
+#
+# device = torch.device('cuda')
+#
+# batch_size = 16
+# image_size = 128
+# n_angles = 128
+#
+# x = cv2.resize(cv2.imread("phantom.png", 0), (image_size, image_size), interpolation=cv2.INTER_CUBIC)
+# x = x.astype(np.float32) / 255
+# # x = generate_random_images(batch_size, image_size)
+# angles = np.linspace(0, np.pi, n_angles).astype(np.float32)
+#
+# astra = AstraWrapper(angles)
+# a_rec = astra.fbp(x)
+# a_rec *= circle_mask(image_size)
+# # a_id, a_y = astra.forward(x)
+# # a_bp = astra.backproject(a_id, image_size, batch_size)
+#
+# with torch.no_grad():
+#     radon = Radon(image_size).to(device)
+#     x = torch.FloatTensor(x).view(1, image_size, image_size)
+#     angles = torch.FloatTensor(angles).to(device)
+#     x_ = x.to(device)
+#     y = radon.forward(x_, angles)
+#     y_ = radon.filter_sinogram(y)
+#     z = radon.backprojection(y_, angles)
+#
+# print(z.size())
+# z *= np.pi / n_angles
+# # a_rec /= np.max(a_rec)
+# print(torch.max(z[0]).item(), np.max(a_rec))
+# print(np.linalg.norm(z[0].cpu().numpy() - a_rec) / np.linalg.norm(a_rec))
+# plt.imshow(z[0].cpu().numpy() - a_rec)
 # plt.figure()
-# plt.imshow(y_[0].cpu().numpy())
+# plt.imshow(a_rec)
 # plt.show()
+# # print(torch.mean((y - y_) ** 2))
+# # print(torch.max(y), torch.max(y_))
+# # plt.imshow(y[0].cpu().numpy())
+# # plt.figure()
+# # plt.imshow(y_[0].cpu().numpy())
+# # plt.show()
+# #
 #
-
-# print("Forward relative error", relative_error(a_y, y.cpu().numpy()))
-# print("Back relative error", relative_error(a_bp, z.cpu().numpy()))
-
-# sinogram_delta = (a_y[0] - y[0].cpu().numpy())**2
-# sinogram_delta /= np.max(sinogram_delta)/255
-# cv2.imwrite("sinogram_delta.png", sinogram_delta.astype(np.uint8))
-
-# radon_noise = RadonNoiseGenerator()
-# print(torch.min(y), torch.max(y))
+# # print("Forward relative error", relative_error(a_y, y.cpu().numpy()))
+# # print("Back relative error", relative_error(a_bp, z.cpu().numpy()))
 #
-# for signal in [2, 3, 4, 8, 10, 12, 14]:
-#     for approximate in [True, False]:
-#         yn = y.clone()
-#         radon_noise.add_noise(yn, signal, 100.0, approximate)
-#         z = radon.backprojection(yn, angles)[0].cpu().numpy()
-#         cv2.imwrite("noised.png", normalize(z))
-#         print(signal, approximate, torch.mean((y - yn)**2).item(), torch.min(yn), torch.max(yn))
+# # sinogram_delta = (a_y[0] - y[0].cpu().numpy())**2
+# # sinogram_delta /= np.max(sinogram_delta)/255
+# # cv2.imwrite("sinogram_delta.png", sinogram_delta.astype(np.uint8))
+#
+# # radon_noise = RadonNoiseGenerator()
+# # print(torch.min(y), torch.max(y))
+# #
+# # for signal in [2, 3, 4, 8, 10, 12, 14]:
+# #     for approximate in [True, False]:
+# #         yn = y.clone()
+# #         radon_noise.add_noise(yn, signal, 100.0, approximate)
+# #         z = radon.backprojection(yn, angles)[0].cpu().numpy()
+# #         cv2.imwrite("noised.png", normalize(z))
+# #         print(signal, approximate, torch.mean((y - yn)**2).item(), torch.min(yn), torch.max(yn))
