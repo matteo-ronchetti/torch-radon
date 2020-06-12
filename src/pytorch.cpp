0b53c14d01b0297fdef20565ae18e164946df568
matteo <mttronchetti@gmail.com>
2020-06-12 18:08:12 +0200

    fixed warning with PyTorch 1.5

--- src/pytorch.cpp
+++ src/pytorch.cpp
@@ -9,7 +9,7 @@
 #include "filtering.h"
 
 
-#define CHECK_CUDA(x) TORCH_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.device() == torch::kCUDA, #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
