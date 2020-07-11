9bd0923340879be259c0c3ba5cfc28b9c12e17b2
matteo <mttronchetti@gmail.com>
2020-07-11 15:36:59 +0200

    .

--- travis/create_build_script.py
+++ travis/create_build_script.py
@@ -1,6 +1,7 @@
 script = [
     "cd /code",
     "PATH=/opt/conda/bin/:$PATH",
+    "conda init"
     "mkdir output"
 ]
 
@@ -13,6 +14,7 @@ for python in ["36", "37"]:
                 f"mkdir -p output/cuda-{cuda}/torch-{torch}",
                 f"conda install -n py{python}cu{cuda.replace('.', '')} pytorch={torch} cudatoolkit={cuda} -c pytorch",
                 f"source activate py{python}cu{cuda.replace('.', '')}",
+                "python --version"
                 f"python build.py clean",
                 f"CUDA_HOME=/usr/local/cuda-10.2 python setup.py bdist_wheel",
                 f"mv dist/*.whl output/cuda-{cuda}/torch-{torch}/"
