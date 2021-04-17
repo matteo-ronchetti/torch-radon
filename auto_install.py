aba8cb967a61133730c3b2040fb519e665868370
Matteo Ronchetti <ronchetti@imfusion.com>
2021-04-17 13:17:01 +0200

    [ci skip] update autoinstall

--- auto_install.py
+++ auto_install.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-version = "1.0.0"
-base_url = "https://rosh-public.s3-eu-west-1.amazonaws.com/radon"
+version = "2.0.0"
+base_url = "https://rosh-public.s3-eu-west-1.amazonaws.com/radon-v2"
 
 green = "\u001b[32m"
 red = "\u001b[31m"
@@ -48,11 +48,11 @@ print("PyTorch:", end=" ")
 if torch_available:
     torch_version = torch.__version__[:3]
     print(torch_version, end=" ")
-    if torch_version in ["1.7", "1.6", "1.5"]:
+    if torch_version in ["1.8", "1.7", "1.6", "1.5"]:
         print(ok)
     else:
         print(error)
-        print("Precompiled packages are build for PyTorch 1.5 to 1.7")
+        print("Precompiled packages are build for PyTorch 1.5 to 1.8")
         print("Consider manually compiling torch-radon")
         sys.exit(1)
 else:
@@ -64,7 +64,7 @@ else:
 cuda_version = torch.version.cuda
 print("CUDA:", cuda_version, end=" ")
 
-if cuda_version in ["10.1", "10.2", "11.0"]:
+if cuda_version in ["10.1", "10.2", "11.0", "11.1"]:
     print(ok)
 else:
     print(error)
@@ -75,6 +75,6 @@ else:
 python_version = f"cp{python_version}-cp{python_version}m" if python_version < 38 else f"cp{python_version}-cp{python_version}"
 
 package_url = f"{base_url}/cuda-{cuda_version}/torch-{torch_version}/torch_radon-{version}-{python_version}-linux_x86_64.whl"
-install_command = f"pip install --force-reinstall {package_url}"
+install_command = f"pip install --force-reinstall --no-cache-dir {package_url}"
 print(f"{blue}Executing: {install_command}{reset}")
 os.system(install_command)
