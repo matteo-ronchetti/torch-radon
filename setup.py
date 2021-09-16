6b6dddf073deb9f70f6459f3a77adbc5d72e2a6b
Philipp Ernst <philipp.ernst@ovgu.de>
2021-09-16 16:10:22 +0200

    add os-specific cuda compilation

--- setup.py
+++ setup.py
@@ -1,14 +1,45 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 import os
-# from make import build
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# cuda_home = os.getenv("CUDA_HOME", "/usr/local/cuda")
-# print(f"Using CUDA_HOME={cuda_home}")
-# build(cuda_home=cuda_home)
+def _create_cuda_extension(os_name: str):
+    if os_name == 'posix':
+        return CUDAExtension(
+            'torch_radon_cuda',
+            [os.path.abspath('src/pytorch.cpp')],
+            include_dirs=[os.path.abspath('include')],
+            library_dirs=[os.path.abspath("objs")],
+            libraries=["m", "c", "gcc", "stdc++", "cufft", "radon"],
+            extra_compile_args=["-fopenmp"],
+            # extra_compile_args=["-static", "-static-libgcc", "-static-libstdc++"],
+            # strip debug symbols
+            extra_link_args=["-Wl,--strip-all"] #, "-static-libgcc", "-static-libstdc++"]
+        )
+
+    if os_name == 'nt':
+        return CUDAExtension(
+            'torch_radon_cuda',
+            [
+                os.path.abspath(os.path.join('src', f))
+                for f in os.listdir('src')
+                if f.endswith('.cpp') or f.endswith('.cu')
+            ],
+            include_dirs=[os.path.abspath('include')],
+            libraries=["cufft"],
+            extra_compile_args={"cxx": ["/std:c++17"]},
+        )
+
+    raise NotImplementedError(f"OS \"{os.name}\" not implemented.")
+
+if os.name == 'posix':
+    from torch.utils.cpp_extension import CUDA_HOME
+    from make import build
+    compiler = os.environ.get('CXX', 'g++')
+    print(f"Using CUDA_HOME={CUDA_HOME}, CXX={compiler}")
+    build(cuda_home=CUDA_HOME, cxx=compiler)
 
 setup(name='torch_radon',
       version="2.0.0",
@@ -24,35 +55,18 @@ setup(name='torch_radon',
           'torch_radon': './torch_radon',
       },
       ext_modules=[
-          CUDAExtension('torch_radon_cuda',
-                        [
-                            os.path.abspath(os.path.join('src', f))
-                            for f in os.listdir('src')
-                            if f.endswith('.cpp') or f.endswith('.cu')
-                        ],
-                        include_dirs=[os.path.abspath('include')],
-                        library_dirs=[os.path.abspath("objs")],
-                        libraries=["cufft"],
-                        extra_compile_args={
-                            "cxx": ["-std=c++17" if os.name=='posix' else "/std:c++17"]
-                        },
-                        # libraries=["m", "c", "gcc", "stdc++", "cufft", "radon"],
-                        # extra_compile_args=["-static", "-static-libgcc", "-static-libstdc++"],
-                        # strip debug symbols
-                        # extra_link_args=["-Wl,--strip-all"] #, "-static-libgcc", "-static-libstdc++"]
-                        )
+          _create_cuda_extension(os.name)
       ],
       cmdclass={'build_ext': BuildExtension},
       zip_safe=False,
       classifiers=[
           "Programming Language :: Python :: 3",
           "Operating System :: POSIX :: Linux",
+          "Operating System :: Microsoft :: Windows",
           "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
       ],
-      install_requires=[
-          "scipy",
-        #   "alpha-transform"  # not available on Windows
-      ],
+      install_requires=["scipy"]
+          + (["alpha-transform"] if os.name=="posix" else []),
       )
 
 
