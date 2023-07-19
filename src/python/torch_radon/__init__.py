b977ac7a086dcb402c2e2d05ba9344d8fbad57bc
Daniel Ching <carterbox@users.noreply.github.com>
2023-07-19 18:43:52 -0500

    DOC: Only track package version in setup.py

--- src/python/torch_radon/__init__.py
+++ src/python/torch_radon/__init__.py
@@ -4,4 +4,14 @@ from .volumes import Volume2D, Volume3D
 from .radon import FanBeam, ParallelBeam, ConeBeam
 from .log_levels import *
 
-__version__ = "2.0"
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ModuleNotFoundError:
+    # Use backport for python<3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("torch_radon")
+except PackageNotFoundError:
+    # package is not installed
+    pass
