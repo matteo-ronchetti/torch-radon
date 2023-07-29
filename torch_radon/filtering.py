0479076637f56bd6748e62dc5f46e626e4be16f1
Yipeng Sun <sypsyp97@gmail.com>
2023-07-29 04:12:44 +0200

    Update filtering.py, rewrite FourierFilters

--- torch_radon/filtering.py
+++ torch_radon/filtering.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from functools import lru_cache
 
 try:
     import scipy.fft
@@ -13,18 +14,15 @@ except ImportError:
 
 class FourierFilters:
     def __init__(self):
-        self.cache = dict()
+        pass
 
     def get(self, size: int, filter_name: str, device):
-        key = (size, filter_name)
-
-        if key not in self.cache:
-            ff = torch.FloatTensor(self.construct_fourier_filter(size, filter_name)).view(1, -1, 1).to(device)
-            self.cache[key] = ff
-
-        return self.cache[key].to(device)
+        filter_name = filter_name.lower()
+        ff = self.construct_fourier_filter(size, filter_name).view(1, 1, -1).to(device)
+        return ff
 
     @staticmethod
+    @lru_cache(maxsize=128)
     def construct_fourier_filter(size, filter_name):
         """Construct the Fourier filter.
 
@@ -50,34 +48,28 @@ class FourierFilters:
                Imaging", IEEE Press 1988.
 
         """
-        filter_name = filter_name.lower()
 
-        n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=np.int),
-                            np.arange(size / 2 - 1, 0, -2, dtype=np.int)))
+        # Initial computations
+        n = np.concatenate((np.arange(1, size // 2 + 1, 2, dtype=np.int),
+                            np.arange(size // 2 - 1, 0, -2, dtype=np.int)))
         f = np.zeros(size)
         f[0] = 0.25
         f[1::2] = -1 / (np.pi * n) ** 2
-
-        # Computing the ramp filter from the fourier transform of its
-        # frequency domain representation lessens artifacts and removes a
-        # small bias as explained in [1], Chap 3. Equation 61
         fourier_filter = 2 * np.real(fftmodule.fft(f))  # ramp filter
-        if filter_name == "ramp" or filter_name == "ram-lak":
-            pass
-        elif filter_name == "shepp-logan":
-            # Start from first element to avoid divide by zero
-            omega = np.pi * fftmodule.fftfreq(size)[1:]
-            fourier_filter[1:] *= np.sin(omega) / omega
-        elif filter_name == "cosine":
-            freq = np.linspace(0, np.pi, size, endpoint=False)
-            cosine_filter = fftmodule.fftshift(np.sin(freq))
-            fourier_filter *= cosine_filter
-        elif filter_name == "hamming":
-            fourier_filter *= fftmodule.fftshift(np.hamming(size))
-        elif filter_name == "hann":
-            fourier_filter *= fftmodule.fftshift(np.hanning(size))
-        else:
-            print(
-                f"[TorchRadon] Error, unknown filter type '{filter_name}', available filters are: 'ramp', 'shepp-logan', 'cosine', 'hamming', 'hann'")
 
-        return fourier_filter
+        # Frequency domain filter adjustments
+        filter_functions = {
+            "ramp": lambda x: x,
+            "ram-lak": lambda x: x,
+            "shepp-logan": lambda x: x[1:] * np.sin(np.pi * fftmodule.fftfreq(size)[1:]) / np.pi * fftmodule.fftfreq(size)[1:],
+            "cosine": lambda x: x * fftmodule.fftshift(np.sin(np.linspace(0, np.pi, size, endpoint=False))),
+            "hamming": lambda x: x * fftmodule.fftshift(np.hamming(size)),
+            "hann": lambda x: x * fftmodule.fftshift(np.hanning(size))
+        }
+        if filter_name not in filter_functions:
+            raise ValueError(f"[TorchRadon] Error, unknown filter type '{filter_name}', available filters are: 'ramp', 'shepp-logan', 'cosine', 'hamming', 'hann'")
+        
+        fourier_filter = filter_functions[filter_name](fourier_filter)
+
+        # Return the first n//2 + 1 elements to match the output of rfft
+        return torch.FloatTensor(fourier_filter[:size//2 + 1])
